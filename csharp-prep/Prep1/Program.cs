ad7aada3d5929787d42dee05e170402caa820fa1
princed18 <dub23003@byui.edu>
2024-02-11 11:02:17 +0200

    asked user for their name

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // asked the user for the name 
+        Console.Write("what is your name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("what s you last name name: ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine("Your name is " + lastName + " , "  + name + " "+ lastName);
     }
 }
