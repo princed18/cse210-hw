03a7bc81eb99dad10c6fb023601b9ed79e223e41
princed18 <dub23003@byui.edu>
2024-02-11 14:08:27 +0200

    guess game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        //int guessNumber = 5;
+        
+        
+        //Console.WriteLine("What is your magic Number: ");
+        //string numberText = Console.ReadLine();
+        //int number = int.Parse(numberText);
+
+        // random nu,ber generation 
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+       int magicGuess = -1;
+
+        while (magicGuess != magicNumber)
+        {
+            Console.WriteLine("What is Your magic number: ");
+            string guessNumberText = Console.ReadLine();
+            magicGuess= int.Parse(guessNumberText);
+
+            if (magicGuess > magicNumber)
+            {
+                Console.WriteLine("Guess low!!");
+            }
+            else if (magicGuess<magicNumber)
+            {
+                Console.WriteLine("Guess high");
+            }
+            else
+            {
+                Console.WriteLine("You have guessed it coreectly!!! cograts !!!");
+            }
+
+
+        }
+
+        
     }
 }
