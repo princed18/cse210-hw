022534e00e6e72b03e9507eb5173261e2d20d7dd
princed18 <dub23003@byui.edu>
2024-02-11 12:47:42 +0200

    prep 2 grade percentage

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,75 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string gradeLetter = "";
+        string sign ="";
+
+        
+
+        Console.WriteLine("What is your grade percentage: ");
+        string gradePercentageText= Console.ReadLine();
+        int gradePercentage = int.Parse(gradePercentageText);
+
+        if (gradePercentage >= 90)
+        {
+
+            gradeLetter = "A";
+        }
+        else if (gradePercentage  >= 80)
+        {
+            gradeLetter= "B";
+        
+        }
+
+        else if ( gradePercentage >= 70)
+        {
+            gradeLetter= "C";
+        }
+        else if ( gradePercentage >= 60)
+        {
+            gradeLetter= "D";
+        }
+
+        else
+        {
+            gradeLetter= "F";
+        }
+
+        int lastDigit = gradePercentage % 10;
+
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        } 
+
+        else if (lastDigit <3)
+        {
+            sign = "-";
+        }
+        else 
+        {
+            sign = "";
+        }
+
+        if (gradeLetter =="A" && lastDigit<7)
+        {
+            sign ="-";
+        }
+
+        else if (gradeLetter == "F" && lastDigit < 7 || lastDigit < 3)
+        {
+            sign = "";
+        }
+        Console.WriteLine("Your grade is " + gradeLetter +sign);
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations you passed your exams!!!");
+        }
+        else 
+        {
+            Console.WriteLine("Unfortunately you failed !! try again next time!!!");
+        }
+
+
     }
 }
